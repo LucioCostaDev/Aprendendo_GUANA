6f7dbc2dd814aea3f4797b84fdc4c80474352b55
Lucio Costa <luciosdc@outlook.com>
2023-05-09 22:52:03 -0300

    alteração

--- ProgramandoJS/Aula12ex/ex015/modelo/script.js
+++ ProgramandoJS/Aula12ex/ex015/modelo/script.js
@@ -3,14 +3,20 @@ function verivficar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.leght == 0 || Number (fano.value) > ano) {
+    if (fano.value.leght == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] verifique os dados e tente novamente!') 
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
-        if (fsex[1].checked) {
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else if (fsex[1].checked) {
             genero = 'Mulher'
-        
-        res.innerHTML = `Detectamos ${genero}` com ${idade} anos. }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero}` com ${idade} anos.` 
+    }
 }
