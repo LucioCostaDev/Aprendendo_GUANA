4dc042cf292e8cdb5b69e45a5552d227ba6f515c
Lucio Costa <luciosdc@outlook.com>
2023-05-09 23:29:48 -0300

    alteração

--- ProgramandoJS/Aula12ex/ex015/modelo/script.js
+++ ProgramandoJS/Aula12ex/ex015/modelo/script.js
@@ -15,29 +15,38 @@ function verificar() {
             genero = 'Homem'
             if (idade >=0 && idade < 1) {
                 //Bebe
-                img.setAttribute('src', 'imagem/bebe.png' )
+                img.setAttribute('src', 'bebe.png')
             } else if (idade < 10) {
                 // Criança
+                img.setAttribute('src', 'criancamasc.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'jovemmasc.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'adultomasc.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'senhor.png')
             }
 
         } else if (fsex[1].checked) {
             genero = 'Mulher'
-            if (idade >=0 && idade < 1) {
+            if (idade >= 0 && idade < 1) {
                 //Bebe
+                img.setAttribute('src', 'bebe.png')
             } else if (idade < 10) {
                 // Criança
+                img.setAttribute('src', 'criancafemi.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'jovemfemi.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'adultafemi.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'senhora.png')
             }
         }
         res.style.textAlign = 'center'
