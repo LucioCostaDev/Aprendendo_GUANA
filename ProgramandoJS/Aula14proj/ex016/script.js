fba767c3b73de42399793fcbd81090ec269d8740
Lucio Costa <luciosdc@outlook.com>
2023-05-15 18:02:40 -0300

    alteração

--- ProgramandoJS/Aula14proj/ex016/script.js
+++ ProgramandoJS/Aula14proj/ex016/script.js
@@ -2,17 +2,32 @@ function contar(){
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.leght == 0 || passo.value.leght == 0) {
-        window.alert('[ERRO] faltam dados!')
+        res.innerHTML = 'Impossivel contar!'
+        // window.alert('[ERRO] faltam dados!')
     } else {
-        res.innerHTML = 'Contador: '
+        res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c +=p) {
-            res.innerHTML += `${c}`
+        if (p <= 0 ) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if(i < f) {
+            // contagem crescente
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F3C3}`
+        }
+    } else {
+            //contagem regressiva
+        for(let c = i; c >= f; c -= p) {
+            res.innerHTML += ` ${c} \u{1F3C3}`
         }
     }
+    res.innerHTML += `\u{1F3C1}`
+ }
 }
+
